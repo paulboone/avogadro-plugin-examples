57ed3cb57677551c19db1e9229952a0d8eb6410c
Paul Boone <narcissus.pong@gmail.com>
2016-10-13 14:52:13 -0400

    add bond_doubler example, remove top-level files

---
+++ bond_doubler/README.md
@@ -0,0 +1,15 @@
+This repository holds example avogadro plugins you can use as a starting point to create your own
+plugins.
+
+Requires Python 3.5+
+
+To test:
+
+```
+pip install -r requirements.txt
+
+./install
+./run --help
+./run --print-dialog < examples/ethane.json
+./run < examples/ethane.json
+```
---
+++ bond_doubler/bond_doubler.py
@@ -0,0 +1,46 @@
+
+def run_transformation(structure, params):
+    """
+    This method performs a transformation on the currently selected structure in avoagadro, using
+    any passed dialog parameters as needed.
+
+    returns a tuple of (status, results) where status = 0 for success, 64 for failure, and the
+    results equal a dictionary of the terms to be returned across stdout.
+    """
+
+    # As an example, we perform a simple (and pointless) reflect and scale operation here.
+    scale = params['Scale']
+    for i, coord in enumerate(structure['atoms']['coords']['3d']):
+        structure['atoms']['coords']['3d'][i] = -1 * scale * coord
+
+    # put together result information
+    # note that you can return a message for display, if there was a result to report or an error.
+    results = {
+        'message_type': None,
+        'message': None,
+        'cjson': structure,
+    }
+
+    status = 0 # success!
+    # status = 64 # fail :(
+
+    return (status, results)
+
+
+def get_dialog_options(structure, params):
+    """
+    This method returns a dictionary containing the option information for anything you want
+    displayed in the dialog. See docs at @TODO.
+
+    A dictionary of the current selected structure, as well as any existing dialog parameters
+    are passed in case your dialog options are context-dependent.
+    """
+    options = {
+        'Scale': {
+            'type': 'integer',
+            'default': 1,
+            'minimum': 1,
+            'maximum': 5,
+        }
+    }
+    return options
