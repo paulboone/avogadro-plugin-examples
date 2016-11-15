8c0e7a1730757f075d5c17c1a8344bd15229a3d7
Paul Boone <narcissus.pong@gmail.com>
2016-11-15 14:01:49 -0500

    implementation of basic fullerene generator

--- scikit-nano/fullerene_generator.py
+++ scikit-nano/fullerene_generator.py
@@ -1,8 +1,10 @@
+
+from avogadro_plugin.utils import extrapolate_bonds
 from sknano.generators import FullereneGenerator
 
 def run_transformation(structure, params):
     """
-    This method performs a transformation on the currently selected structure in avoagadro, using
+    This method performs a transformation on the currently selected structure in avogadro, using
     any passed dialog parameters as needed.
 
     returns a tuple of (status, results) where status = 0 for success, 64 for failure, and the
@@ -10,6 +12,11 @@ def run_transformation(structure, params):
     """
 
     fg = FullereneGenerator(N=params['N'])
+
+    structure["atoms"]["elements"]["number"] =  [ a.Z for a in fg.atoms ]
+    structure["atoms"]["coords"]["3d"] = [ c for a in fg.atoms for c in (a.x, a.y, a.z) ]
+    atoms = [ (a.x, a.y, a.z, a.Z) for a in fg.atoms ]
+    structure["bonds"]["connections"]["index"] = extrapolate_bonds(atoms)
 
     # put together result information
     # note that you can return a message for display, if there was a result to report or an error.
@@ -36,8 +43,8 @@ def get_dialog_options(structure, params):
     options = [{
         'name': 'N',
         'type': 'integer',
-        'default': 1,
-        'minimum': 1,
-        'maximum': 500,
+        'default': 20,
+        'minimum': 20,
+        'maximum': 100,
     }]
     return options
